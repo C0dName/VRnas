fe25e673680a47a8c2ff687e37aa40659b6a8c48
C0dName <tortum1996@gmail.com>
2025-08-12 15:19:21 +0300

    добавил первый всплывающий при скролле элемент Main1Dop.jsx

--- src/app/components/Main1Dop.jsx
+++ src/app/components/Main1Dop.jsx
@@ -1,9 +1,53 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useRef, useState } from 'react';
+import classes from './Main1Dop.module.css';
 
 const Main1Dop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+    const ref = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                    observer.unobserve(entry.target); // Отключаем наблюдение после появления
+                }
+            },
+            { threshold: 0.4 } // Срабатывает, когда 10% элемента видно
+        );
+
+        if (ref.current) observer.observe(ref.current);
+
+        return () => observer.disconnect();
+    }, []);
     return (
-        <div>
-            
+        <div ref={ref}
+            className={`${classes.main1DopGlobalPosition} ${isVisible ? classes.show : ''}`}>
+            <div className={classes.main1DopBlock}>
+                <img src="/Main1DopImg1.svg" alt="пикча с ВР шлемом"/>
+                <p className={classes.main1DopTextTop}>Expertise</p>
+                <p className={classes.main1DopTextBottom}>Our team consists of experienced VR developers,
+                    designers, and technicians who have a passion for
+                    VR and a commitment to delivering quality work
+                    and give the best service</p>
+            </div>
+            <div className={classes.main1DopBlock}>
+                <img src="/Main1DopImg2.svg" alt="пикча с 3д эксползицией"/>
+                <p className={classes.main1DopTextTop}>Customization</p>
+                <p className={classes.main1DopTextBottom}>Every client is unique, and we believe every VR
+                    experience should be too. We'll work with you to
+                    create a customized solution that meets your
+                    specific needs and goals</p>
+            </div>
+            <div className={classes.main1DopBlock}>
+                <img src="/Main1DopImg3.svg" alt="пикча с челиком в шлеме"/>
+                <p className={classes.main1DopTextTop}>Service</p>
+                <p className={classes.main1DopTextBottom}>We believe in providing exceptional customer
+                    service, from initial consultation to final delivery.
+                    Our goal is to ensure you're satisfied with every
+                    aspect of your VR experience.</p>
+            </div>
         </div>
     );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Header from './components/Header'
 import Main1 from './components/Main1'
+import Main1Dop from './components/Main1Dop'
 import classes from './PageHome.module.css'
 
 
@@ -8,6 +9,7 @@ export default function Home() {
     return (
         <div className={classes.pageHomePosition}>
             <Main1/>
+            <Main1Dop/>
             {/*<h1 style={{marginTop: "2000px"}}>penis</h1>*/}
         </div>
     )
